b287f2a526984ccc5ed4d266451e788d8e5a97b9
Greg <gmolsenvideo@gmail.com>
2017-09-14 16:05:32 -0400

    automated code testing procedure

---
+++ AutomatedCodeTesting/Fraction.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AutomatedCodeTesting {
+	public class Fraction {
+		public int Numerator { get; set; }
+
+		public int _denominator;
+		public int Denominator {
+			get {
+				return _denominator;
+			}
+			set {
+				if(value == 0) {
+					throw new DivideByZeroException("Why don't you just give up?");
+				}
+				_denominator = value;
+			}
+		}
+
+		public double ToDecimal() {
+			return (double)Numerator / (double)Denominator;
+		}
+
+		}
+	}
---
+++ TestAutomatedCodeTesting/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutomatedCodeTesting;
+using System.Diagnostics;
+
+namespace TestAutomatedCodeTesting {
+	class Program {
+		static void Main(string[] args) {
+			new Program().Run();
+		}
+		void Run() {
+			Fraction fraction = new Fraction();
+			fraction.Numerator = 100;
+			fraction.Denominator = -100;
+			var aDecimal = fraction.ToDecimal();
+			var Passed = (aDecimal == -1);
+			Debug.WriteLine($"The test passed : {Passed}");
+
+			fraction.Numerator = 0;
+			try {
+				Passed = false;
+				fraction.Denominator = 0;
+			} catch (DivideByZeroException) {
+				Passed = true;
+			}
+			Debug.WriteLine($"The test passed : {Passed}");
+			
+		}
+	}
+}
